92463e4fa54686704215f3be12fe8b35ac1c3c65
Potey Charles <ppsjcfullstackdeveloper@gmail.com>
2024-10-12 12:26:21 +0000

    Making templates of homepage, signup and login page with bootstrap

---
+++ templates/homepage.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+       <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>RecipeShare - L'application de partage de recette</title>
+       <link rel="shortcut icon" href="icons/recipe-share.png" type="image/x-icon">
+       <link rel="stylesheet" href="css/main.css">
+    </head>
+
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="text-center">
+                        <img src="icons/recipe-share.png" width="300" alt="recipe share's icon">
+                    </div>
+                    <h1 class="text-center">L'application de partage de recette</h1>
+                    <p class="text-secondary fs-5 mt-4" style="text-align: justify;">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Sit non a quae, voluptas aperiam eum laudantium inventore atque quisquam
+                        odio recusandae excepturi temporibus. Hic at tempore praesentium atque molestiae.
+                        Eum.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Sit non a quae, voluptas aperiam eum laudantium inventore atque quisquam
+                        odio recusandae excepturi temporibus. Hic at tempore praesentium atque molestiae.
+                        Eum.
+                    </p>
+                    <div class="text-center mt-4">
+                        <a href="src/controllers/signup.php" class="btn btn-primary">Inscription</a>
+                        <a href="src/controllers/login.php" class="btn btn-warning text-light">Connexion</a>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <img src="images/img-home.png" alt="" class="img-fluid ms-3">
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
